59d73beb4271a24bf6ab748985da24f98d54e372
ryosuke takasawa <ten.r0619@gmail.com>
2020-09-06 05:43:26 +0900

    commit

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module DbSample
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.active_record.writing_role = :writable
+    config.active_record.reading_role = :readonly
   end
 end
